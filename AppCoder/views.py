63b43603425cff4000b3bc1ea960f75589c2db11
Lautaro Lombardo <lauty.lombardo@hotmail.com>
2022-05-27 12:27:38 -0300

    commit

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-from Proyecto1.ProyectoCoder.AppCoder.models import Curso
+from AppCoder.models import Curso
 
 # Create your views here.
-def curso(self):
-    curso = Curso(nombre="Desarrollo wev", camada = "19881")
-    curso.save()
-    documentoDeTexto = f"C--->Curso: {curso.nombre} Camada: {curso.camada}"
+def curso(request):
+    return render(request, "AppCoder/cursos.html")
 
-    return HttpResponse(documentoDeTexto)
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
+
+def miPlantilla(request):
+    return render(request, "AppCoder/miPlantilla.html")
